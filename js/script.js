db73cee38a6e3d39b0ef00114db47dbb966c367d
Verta-master <vertyagina@gmail.com>
2022-07-14 21:51:23 +0600

    mobile

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 //Mobile menu
-//$('.menu__btn').click(function() {
-//  $('.menu').toggleClass('menu--opened');
-//  $('.menu__list').slideToggle();
-//})
-//
-//if (window.innerWidth < 766) {
-//  $('.menu__link').click(function() {
-//    $('.menu__list').slideToggle();
-//    $('.menu').removeClass('menu--opened');
-//  })
-//}
+if (window.innerWidth < 1200) {
+  $('.mobile__link--menu').click(function() {
+    event.preventDefault();
+    $('.menu-mobile').addClass('menu-mobile--open');
+  })
+  
+  $('.menu-mobile__close').click(function() {
+    $('.menu-mobile').removeClass('menu-mobile--open');
+  })
+  
+  $('.menu-mobile__btn').click(function() {
+    $(this).next().slideToggle();
+    $(this).toggleClass('menu-mobile__btn--top');
+    $('.menu-mobile__wrap').scroll(function(){
+      $(".menu-mobile__wrap").getNiceScroll().resize();
+    });
+  })
+  
+  $('.menu-mobile__wrap').niceScroll({
+    cursorcolor: '#2647E6',
+  });
+}
 
 //Mobile footer
 $('.footer__down').click(function() {
